f290d20f85241ff2ec46fcc1b88aedbebba58e26
Alex Higgs <alex.higgs@data-vault.com>
2024-03-19 14:29:41 +0000

    Add Satellite

---
+++ src/models/stage/stg_emails.sql
@@ -0,0 +1,27 @@
+{%- set yaml_metadata -%}
+source_model: 
+  raw_emails: Sheet1
+derived_columns:
+  RECORD_SOURCE: '!MARKETING_MAILING_LIST'
+  LOAD_DATETIME: '_y42_extracted_at'
+hashed_columns:
+  CUSTOMER_PK: 'C_CUSTKEY'
+  CUSTOMER_HASHDIFF:
+    is_hashdiff: true
+    columns:
+      - 'EMAIL'
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{% set source_model = metadata_dict['source_model'] %}
+
+{% set derived_columns = metadata_dict['derived_columns'] %}
+
+{% set hashed_columns = metadata_dict['hashed_columns'] %}
+
+{{ automate_dv.stage(include_source_columns=true,
+                     source_model=source_model,
+                     derived_columns=derived_columns,
+                     hashed_columns=hashed_columns,
+                     ranked_columns=none) }}
